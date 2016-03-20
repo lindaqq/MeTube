4d01804bc67091118ab131a457a375297c7401b5
Qi Xiao <qixiao2017@gmail.com>
2016-03-20 00:28:14 -0400

    add user account page

---
+++ public/account.php
@@ -0,0 +1,11 @@
+<?php
+
+    // configuration
+    require("../includes/config.php"); 
+    
+    $rows = query("SELECT * FROM account WHERE id = ?", $_SESSION["id"]);
+    
+    render("account_form.php", ["user" => $rows[0]]);
+
+?>
+
---
+++ templates/account_form.php
@@ -0,0 +1,47 @@
+<!--div class="col-lg-6 col-sm-6"-->
+<div class="container">
+    <div class="card hovercard">
+        <div class="card-background">
+            <img class="card-bkimg" alt="" src="../templates/images/orange_background.jpg">
+        </div>
+        <div class="useravatar">
+            <img alt="" src="../templates/images/account.png">
+        </div>
+        <div class="card-info"> <span class="card-title"><?php 
+            $username = $user["username"];
+            echo $username;
+            ?></span>
+
+        </div>
+    </div>
+    <div class="btn-pref btn-group btn-group-justified btn-group-lg" role="group" aria-label="...">
+        <div class="btn-group" role="group">
+            <button type="button" id="stars" class="btn btn-primary" href="#tab1" data-toggle="tab"><span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+                <div class="hidden-xs">Stars</div>
+            </button>
+        </div>
+        <div class="btn-group" role="group">
+            <button type="button" id="favorites" class="btn btn-default" href="#tab2" data-toggle="tab"><span class="glyphicon glyphicon-heart" aria-hidden="true"></span>
+                <div class="hidden-xs">Favorites</div>
+            </button>
+        </div>
+        <div class="btn-group" role="group">
+            <button type="button" id="following" class="btn btn-default" href="#tab3" data-toggle="tab"><span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+                <div class="hidden-xs">Following</div>
+            </button>
+        </div>
+    </div>
+
+        <div class="well">
+      <div class="tab-content">
+        <div class="tab-pane fade in active" id="tab1">
+          <h3>This is tab 1</h3>
+        </div>
+        <div class="tab-pane fade in" id="tab2">
+          <h3>This is tab 2</h3>
+        </div>
+        <div class="tab-pane fade in" id="tab3">
+          <h3>This is tab 3</h3>
+        </div>
+      </div>
+    </div>
