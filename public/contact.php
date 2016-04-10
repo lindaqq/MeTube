613502fd12e4c534f3661828a9f098a43718aaf7
Qi Xiao <qixiao2017@gmail.com>
2016-04-10 10:51:40 -0400

    test db connect

--- public/contact.php
+++ public/contact.php
@@ -4,6 +4,8 @@
     require("../includes/config.php"); 
     require("../includes/accountService.php");
 
+    $db = new mysql_db(SERVER, USERNAME, PASSWORD, DATABASE);
+
     if ($_SERVER["REQUEST_METHOD"] == "POST"){
         if(!emtpy($_POST["contact"])){
             
@@ -15,14 +17,17 @@
             
         }
         
+        
     }
     else{
         $contacts = getContacts($_SESSION["username"]);
         $friends = getFriends($_SESSION["username"]);
         $foes = getFoes($_SESSION["username"]);
         
+        //$db.sql_close();
         render("contact_template.php", ["contacts" => $contacts, "friends" => $friends, "foes" => $foes,  "errortext" => "","titile" => "Contact"]);
     }
+
 
 ?>
 
