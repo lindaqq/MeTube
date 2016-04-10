6643d16e1e27e42b6efddfe239d43386366d1cd2
Qi Xiao <qixiao2017@gmail.com>
2016-04-10 18:54:39 -0400

    working on displaying single image page

--- public/index.php
+++ public/index.php
@@ -9,3 +9,4 @@ $images = query("SELECT * FROM media order by viewcount DESC limit 6");
 // render home page for image, can be changed to video later
 render("image_homepage.php", ["images" => $images]);
 ?>
+
