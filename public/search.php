8833c5f1a1cbac9b4e7e7ccd80d5f5dc5770cdd5
Fangyu <fangyuh@clemson.edu>
2016-04-10 18:53:44 -0400

    search control

--- public/search.php
+++ public/search.php
@@ -7,8 +7,31 @@ $db = new mysql_db(SERVER, USERNAME, PASSWORD,DATABASE);
 
 // $_POST["keywords"] should be the search keyword
 
-$result = search($_POST["keywords"]); 
-  render("search_template.php", ["title" => "Search Result", "result" => $result]);
+$key = "";
+if (!isset($_POST["keywords"])) {
+  $words = $_POST["key"];
+  $key = $words;
+  $type = $_POST["type"];
+  $category = $_POST["category"];
+
+  $time = $_POST["date"];
+  if ($time == 1) {
+    $start= (new \DateTime())->modify('-24 hours');
+  $starttime = $start->format('Y-m-d H:i:s');
+  } else if($time == 2) {
+    $start= (new \DateTime())->modify('-30 days');
+  $starttime = $start->format('Y-m-d H:i:s');
+  } else {
+    $starttime= "";
+  }
+
+  $result = filterSearch($words, $type, $category, $starttime); 
+} else {
+  $key = $_POST["keywords"];
+  $result = search($_POST["keywords"]); 
+}
+
+  render("search_template.php", ["title" => "Search Result", "result" => $result, "key" => $key]);
 $db->sql_close();
 ?>
 
