fa6135f406a31b99abfba172f4270b06a81d2f83
Qi Xiao <qixiao2017@gmail.com>
2016-04-10 19:30:32 -0400

    finish image homepage

--- templates/image_homepage.php
+++ templates/image_homepage.php
@@ -33,29 +33,49 @@ _END;
 <p class="lead"> Popular Uploads: </p>
 <div class="row text-center">
     <?php
-         for($i=0; $i < 12; $i++){
+         //for($i=0; $i < 12; $i++){
+        foreach($populars as $popular){
+            $id=$popular["mediaid"];
+            $title = $popular["title"];
+            $viewcount = $popular["viewcount"];
+            $posttime = $popular["posttime"];
+            $posttime = split(' ', $posttime)[0];
                echo <<<_END
+               
             <div class="col-sm-4 col-md-4 col-lg-4 col-xs-6">
-
-      			<div class="img-thumbnail"> <a href="../public/image.php?id=1"><img src="../templates/images/400X200.gif" alt="Thumbnail Image 1" class="img-responsive"></a></div>
-      			<p>image name</p>
-    		</div> 
+                <div class = "panel panel-default">
+      			   <div class="img-thumbnail"> <a href="../public/image.php?id=$id"><img src="../templates/images/400X200.gif" alt="Thumbnail Image 1" class="img-responsive"></a></div>
+      			   <p>$title</p>
+                    <h6>viewed: $viewcount</h6>
+                    <h6>uploaded at: $posttime</h6>
+    		  </div> 
+            </div>
 _END;
         }   
     ?>
             
 </div>
 
+<br><br>
 <p class="lead"> Recent Uploads: </p>
 <div class="row text-center">
     <?php
-         for($i=0; $i < 12; $i++){
+         //for($i=0; $i < 12; $i++){
+        foreach($recents as $recent){
+            $id=$recent["mediaid"];
+            $title = $recent["title"];
+            $viewcount = $recent["viewcount"];
+            $posttime = $recent["posttime"];
+            $posttime = split(' ', $posttime)[0];
                echo <<<_END
             <div class="col-sm-4 col-md-4 col-lg-4 col-xs-6">
-
-      			<div class="img-thumbnail"> <a href="../public/image.php?id=1"><img src="../templates/images/400X200.gif" alt="Thumbnail Image 1" class="img-responsive"></a></div>
-      			<p>image name</p>
-    		</div> 
+                <div class = "panel panel-default">
+      			   <div class="img-thumbnail"> <a href="../public/image.php?id=$id"><img src="../templates/images/400X200.gif" alt="Thumbnail Image 1" class="img-responsive"></a></div>
+      			   <p>$title</p>
+                    <h6>viewed: $viewcount</h6>
+                    <h6>uploaded at: $posttime</h6>
+    		      </div> 
+            </div>
 _END;
         }   
     ?>
