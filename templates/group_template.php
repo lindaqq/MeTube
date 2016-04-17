d88b775c949d2b8d801ac5b7ba42c54358cc8187
Qi Xiao <qixiao2017@gmail.com>
2016-04-17 14:05:10 -0400

    subscribe

--- templates/group_template.php
+++ templates/group_template.php
@@ -5,14 +5,14 @@
             <?php               
                 foreach($groups as $group) {
                   $groupname = $group["groupname"];
-                  $detail = $group["detail"];
+                  $topic = $group["topic"];
                     echo <<<_END
                     <a href="#" class="list-group-item">
                         <h4 class="list-group-item-heading">
                            
                                 $groupname
                         </h4>
-                        <p class="list-group-item-text">$detail</p>
+                        <p class="list-group-item-text">$topic</p>
                         <br>
                         <div class="list-group-item-text">
                             <button onclick="document.location.href = '../public/topic.php'" class="btn btn-primary btn-primary" type="button">Join</button>
@@ -28,10 +28,10 @@ _END;
     <div class="col-sm-6 col-md-6 col-lg-6">
         <p>Create a new group:</p>
         <form class="form-signin" action="../public/group.php" method="post">
-                <input type="text" class="form-control" name="groupname" placeholder="group name" maxlength="20" required autofocus>
+                <input type="text" class="form-control" name="groupname" placeholder="group name" maxlength="30" required autofocus>
                 <br>
-                <textarea  class="form-control" name="detail" rows="4" cols="50" placeholder="group description" required></textarea>
-                
+                <input type="text" class="form-control" name="topic" placeholder="topic" maxlength="30" required>
+                   
                     <br><br>
                 <button class="btn btn-lg btn-primary btn-block" type="submit">
                     Create</button>
